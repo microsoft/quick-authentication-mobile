b8be387466bb4bc37ea68217ab790b0dbbd65acd
jingjinggu <jingjinggu@microsoft.com>
2022-08-23 21:23:09 +0800

    add sign in button style setting pop

--- android/Demo/src/main/java/com/example/signdemo1/SignInActivity.java
+++ android/Demo/src/main/java/com/example/signdemo1/SignInActivity.java
@@ -2,55 +2,44 @@ package com.example.signdemo1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.example.signdemo1.view.SignInButtonSettingPop;
 import com.microsoft.quick.auth.signin.MQASignInClient;
 import com.microsoft.quick.auth.signin.SignInClient;
 import com.microsoft.quick.auth.signin.entity.AccountInfo;
-import com.microsoft.quick.auth.signin.view.ButtonLogoAlignment;
-import com.microsoft.quick.auth.signin.view.ButtonShape;
-import com.microsoft.quick.auth.signin.view.ButtonSize;
 import com.microsoft.quick.auth.signin.view.ButtonText;
-import com.microsoft.quick.auth.signin.view.ButtonTheme;
-import com.microsoft.quick.auth.signin.view.ButtonType;
 import com.microsoft.quick.auth.signin.view.MQASignInButton;
 
 public class SignInActivity extends Activity {
 
     private MQASignInButton mSignInButton;
-    private RadioGroup mTypeRadioGroup;
-    private RadioGroup mThemeRadioGroup;
-    private RadioGroup mSizeRadioGroup;
-    private RadioGroup mTextRadioGroup;
-    private RadioGroup mShapeRadioGroup;
-    private RadioGroup mAlignmentRadioGroup;
     private TextView mStatus;
     private TextView mUserInfoResult;
     private ImageView mUserPhoto;
+    private View mSignButtonSetting;
+    private ViewGroup mRootView;
 
     private SignInClient mSignInClient;
     private boolean mSignedIn;
+    private SignInButtonSettingPop pop;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         mSignInButton = findViewById(R.id.ms_sign_button);
+        mRootView = findViewById(R.id.root_view);
         mStatus = findViewById(R.id.status);
         mUserInfoResult = findViewById(R.id.userInfoResult);
         mUserPhoto = findViewById(R.id.userPhoto);
-        mTypeRadioGroup = findViewById(R.id.sign_button_type_radio_group);
-        mThemeRadioGroup = findViewById(R.id.sign_button_theme_radio_group);
-        mSizeRadioGroup = findViewById(R.id.sign_button_size_radio_group);
-        mTextRadioGroup = findViewById(R.id.sign_button_text_radio_group);
-        mShapeRadioGroup = findViewById(R.id.sign_button_shape_radio_group);
-        mAlignmentRadioGroup = findViewById(R.id.sign_button_logo_alignment_radio_group);
-
-        initRadioGroup();
+        mSignButtonSetting = findViewById(R.id.ms_sign_button_setting);
 
         mSignInClient = new MQASignInClient(this);
         mSignInButton.setOnClickListener(v -> {
@@ -65,6 +54,13 @@ public class SignInActivity extends Activity {
             } else {
                 mSignInClient.signOut((aBoolean, error) -> uploadSignInfo(null, error));
             }
+        });
+        mSignButtonSetting.setOnClickListener(v -> {
+            if (pop != null && pop.isShowing()) return;
+            if (pop == null) {
+                pop = new SignInButtonSettingPop(this, mSignInButton);
+            }
+            pop.showAtLocation(mRootView, Gravity.BOTTOM, 0, 0);
         });
         getCurrentAccount();
     }
@@ -96,57 +92,4 @@ public class SignInActivity extends Activity {
         mSignInButton.setButtonText(signIn ? ButtonText.SIGN_OUT : ButtonText.SIGN_IN_WITH);
     }
 
-    private void initRadioGroup() {
-        mTypeRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            mSignInButton.setButtonType(checkedId == R.id.standard ? ButtonType.STANDARD :
-                    ButtonType.ICON);
-        });
-        mThemeRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            mSignInButton.setButtonTheme(checkedId == R.id.filled_light ? ButtonTheme.FILLED_BLUE :
-                    ButtonTheme.FILLED_BLACK);
-        });
-        mSizeRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            switch (checkedId) {
-                case R.id.size_small:
-                    mSignInButton.setButtonSize(ButtonSize.SMALL);
-                    break;
-                case R.id.size_medium:
-                    mSignInButton.setButtonSize(ButtonSize.MEDIUM);
-                    break;
-                default:
-                    mSignInButton.setButtonSize(ButtonSize.LARGE);
-                    break;
-            }
-        });
-        mTextRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            mSignInButton.setButtonText(checkedId == R.id.signin_with ? ButtonText.SIGN_IN_WITH :
-                    ButtonText.SIGN_OUT);
-        });
-        mShapeRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            switch (checkedId) {
-                case R.id.rounded:
-                    mSignInButton.setButtonShape(ButtonShape.ROUNDED);
-                    break;
-                case R.id.pill:
-                    mSignInButton.setButtonShape(ButtonShape.PILL);
-                    break;
-                default:
-                    mSignInButton.setButtonShape(ButtonShape.RECTANGULAR);
-                    break;
-            }
-        });
-        mAlignmentRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            switch (checkedId) {
-                case R.id.icon_left_text_center:
-                    mSignInButton.setButtonLogoAlignment(ButtonLogoAlignment.ICON_LEFT_TEXT_CENTER);
-                    break;
-                case R.id.center:
-                    mSignInButton.setButtonLogoAlignment(ButtonLogoAlignment.CENTER);
-                    break;
-                default:
-                    mSignInButton.setButtonLogoAlignment(ButtonLogoAlignment.LEFT);
-                    break;
-            }
-        });
-    }
 }
