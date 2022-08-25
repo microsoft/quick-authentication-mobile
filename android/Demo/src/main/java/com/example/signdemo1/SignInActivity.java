57c8c34b656fc7eb7c22e461357a006ecb2f3ff6
jingjinggu <jingjinggu@microsoft.com>
2022-08-25 18:22:33 +0800

    merge token to sign in page

--- android/Demo/src/main/java/com/example/signdemo1/SignInActivity.java
+++ android/Demo/src/main/java/com/example/signdemo1/SignInActivity.java
@@ -11,9 +11,12 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.example.signdemo1.view.SignInButtonSettingPop;
+import com.microsoft.identity.client.exception.MsalUiRequiredException;
 import com.microsoft.quick.auth.signin.MSQASignInClientImp;
 import com.microsoft.quick.auth.signin.MSQASignInClient;
+import com.microsoft.quick.auth.signin.callback.OnCompleteListener;
 import com.microsoft.quick.auth.signin.entity.AccountInfo;
+import com.microsoft.quick.auth.signin.entity.ITokenResult;
 import com.microsoft.quick.auth.signin.view.ButtonText;
 import com.microsoft.quick.auth.signin.view.MSQASignInButton;
 
@@ -24,11 +27,15 @@ public class SignInActivity extends Activity {
     private TextView mUserInfoResult;
     private ImageView mUserPhoto;
     private View mSignButtonSetting;
+    private TextView mTokenResult;
+    private View mSignOutButton;
+    private View msAcquireTokenButton;
     private ViewGroup mRootView;
 
     private MSQASignInClient mSignInClient;
-    private boolean mSignedIn;
+    private AccountInfo mAccountInfo;
     private SignInButtonSettingPop pop;
+    private String[] scops;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,20 +47,24 @@ public class SignInActivity extends Activity {
         mUserInfoResult = findViewById(R.id.userInfoResult);
         mUserPhoto = findViewById(R.id.userPhoto);
         mSignButtonSetting = findViewById(R.id.ms_sign_button_setting);
+        mSignOutButton = findViewById(R.id.ms_sign_out_button);
+        mTokenResult = findViewById(R.id.tv_token_result);
+        msAcquireTokenButton = findViewById(R.id.ms_acquire_token_button);
+        scops = new String[]{"user.read"};
 
         mSignInClient = new MSQASignInClientImp(this);
         mSignInButton.setOnClickListener(v -> {
-            if (!mSignedIn) {
-                mSignInClient.signIn(this, (accountInfo, error) -> {
-                    if (accountInfo != null) {
-                        uploadSignInfo(accountInfo, null);
-                    } else {
-                        uploadSignInfo(null, error);
-                    }
-                });
-            } else {
-                mSignInClient.signOut((aBoolean, error) -> uploadSignInfo(null, error));
-            }
+            mSignInClient.signIn(this, (accountInfo, error) -> {
+                if (accountInfo != null) {
+                    uploadSignInfo(accountInfo, null);
+                } else {
+                    uploadSignInfo(null, error);
+                }
+            });
+        });
+        mSignOutButton.setOnClickListener(v -> {
+            mSignInClient.signOut((aBoolean, error) -> uploadSignInfo(null, error));
+            updateTokenResult(null, null);
         });
         mSignButtonSetting.setOnClickListener(v -> {
             if (pop != null && pop.isShowing()) return;
@@ -62,7 +73,35 @@ public class SignInActivity extends Activity {
             }
             pop.showAtLocation(mRootView, Gravity.BOTTOM, 0, 0);
         });
+        msAcquireTokenButton.setOnClickListener(v -> {
+            if (mAccountInfo != null) {
+                mSignInClient.acquireTokenSilent(mAccountInfo, scops, (iTokenResult, error) -> {
+                    /**
+                     * If acquireTokenSilent() returns an error that requires an interaction
+                     * (MsalUiRequiredException),
+                     * invoke acquireToken() to have the user resolve the interrupt interactively.
+                     *
+                     * Some example scenarios are
+                     *  - password change
+                     *  - the resource you're acquiring a token for has a stricter set of requirement than
+                     *  your Single Sign-On refresh token.
+                     *  - you're introducing a new scope which the user has never consented for.
+                     */
+                    if (error instanceof MsalUiRequiredException) {
+                        acquireToken();
+                    } else {
+                        updateTokenResult(iTokenResult, error);
+                    }
+                });
+            } else {
+                acquireToken();
+            }
+        });
         getCurrentAccount();
+    }
+
+    private void acquireToken() {
+        mSignInClient.acquireToken(this, scops, null, this::updateTokenResult);
     }
 
     private void uploadSignInfo(AccountInfo accountInfo, Exception error) {
@@ -78,6 +117,7 @@ public class SignInActivity extends Activity {
             mUserPhoto.setImageBitmap(null);
             mUserInfoResult.setText(error != null ? "login error: " + error.getMessage() : "");
         }
+        mAccountInfo = accountInfo;
         updateStatus(accountInfo != null);
     }
 
@@ -87,9 +127,15 @@ public class SignInActivity extends Activity {
     }
 
     private void updateStatus(boolean signIn) {
-        mSignedIn = signIn;
         mStatus.setText(signIn ? "signed in" : "signed out");
         mSignInButton.setButtonText(signIn ? ButtonText.SIGN_OUT : ButtonText.SIGN_IN_WITH);
+
+        mSignInButton.setVisibility(signIn ? View.GONE : View.VISIBLE);
+        mSignOutButton.setVisibility(signIn ? View.VISIBLE : View.GONE);
     }
 
+    private void updateTokenResult(ITokenResult iTokenResult, Exception error) {
+        mTokenResult.setText(iTokenResult != null ? iTokenResult.getAccessToken() : error != null ?
+                "error:" + error.getMessage() : "");
+    }
 }
