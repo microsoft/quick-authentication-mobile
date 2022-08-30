b8a31141dc7fe46b51c72e1fa38fab81a0ebe9df
jingjinggu <jingjinggu@microsoft.com>
2022-08-30 22:18:15 +0800

    merge sign in error

--- android/quick_auth/src/main/java/com/microsoft/quick/auth/signin/signinclient/ISignInClientHolder.java
+++ android/quick_auth/src/main/java/com/microsoft/quick/auth/signin/signinclient/ISignInClientHolder.java
@@ -1,7 +1,5 @@
 package com.microsoft.quick.auth.signin.signinclient;
 
-import androidx.annotation.WorkerThread;
-
 public interface ISignInClientHolder {
     ISignInClientApplication getClientApplication() throws Exception;
 
