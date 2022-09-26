aa9fa67850824480474f8028b0e581c8ca9cf918
Minggang Wang <minggangwang@microsoft.com>
2022-09-26 17:24:40 +0800

    [iOS] Implement the telemetry functionality
    This patch implements the following metrics:
    - SignIn.Success
    - SignIn.Failure
    - button-sign-in
    - signOut
    - getCurrentAccount
    - signIn
    - acquireToken
    - acquireTokenSilent
    
    Please reference the following design for details:
    https://microsoft.sharepoint.com/:w:/t/Edge/EbrIVsQr3C9KszRE8yoi_TABHt8qBR1ao7XW1tyKw8pTSw?e=YkNaCQ
    
    Related work item: 41300769

---
+++ iOS/MSQASignIn/src/MSQATelemetrySender.m
@@ -0,0 +1,117 @@
+//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+#import "MSQATelemetrySender.h"
+
+static NSString *const kMetricURL = @"https://edge-auth.microsoft.com/metric";
+
+static NSString *const kContentType = @"application/json";
+
+static NSString *const kOrigin = @"https://edge-auth.microsoft.com/";
+
+@interface MSQATelemetrySender ()
+
+@property(nonatomic, readonly, nullable) NSString *uuid;
+
+@property(nonatomic, readonly, nullable) NSString *appVersion;
+
+@property(nonatomic, readonly, nullable) NSString *timestamp;
+
+@end
+
+@implementation MSQATelemetrySender
+
++ (instancetype)sharedInstance {
+  static dispatch_once_t once;
+  static MSQATelemetrySender *sharedInstance;
+  dispatch_once(&once, ^{
+    sharedInstance = [[self alloc] initInternal];
+  });
+  return sharedInstance;
+}
+
+- (instancetype)initInternal {
+  if (!(self = [super init])) {
+    return nil;
+  }
+  _uuid = [[NSUUID UUID] UUIDString];
+  return self;
+}
+
+- (void)sendWithEvent:(NSString *)event message:(NSString *)message {
+  NSMutableURLRequest *urlRequest = [NSMutableURLRequest new];
+  urlRequest.URL = [NSURL URLWithString:kMetricURL];
+  urlRequest.HTTPMethod = @"POST";
+  urlRequest.allHTTPHeaderFields =
+      @{@"Content-Type" : kContentType, @"Origin" : kOrigin};
+
+  NSData *data = [self createTelemetryDataWithEvent:event message:message];
+  if (data) {
+    [urlRequest setHTTPBody:data];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session
+        dataTaskWithRequest:urlRequest
+          completionHandler:^(NSData *_Nullable data,
+                              NSURLResponse *_Nullable response,
+                              NSError *_Nullable error) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            if (error || httpResponse.statusCode >= 400) {
+              // TODO(minggang): Replace `NSLog` with `MSQALogger`.
+              NSLog(@"Sending telemetry failed.");
+              return;
+            }
+          }];
+    [task resume];
+  }
+}
+
+- (NSString *)timestamp {
+  NSDate *date = [NSDate date];
+  NSISO8601DateFormatter *formatter = [[NSISO8601DateFormatter alloc] init];
+  return [formatter stringFromDate:date];
+}
+
+- (NSString *)appVersion {
+  return [[[NSBundle mainBundle] infoDictionary]
+      objectForKey:@"CFBundleShortVersionString"];
+}
+
+- (NSData *)createTelemetryDataWithEvent:(NSString *)event
+                                 message:(NSString *)message {
+  NSDictionary *eventsDict = [[NSDictionary alloc]
+      initWithObjects:@[ event, @1, message, self.timestamp ]
+              forKeys:@[ @"EventName", @"Count", @"Message", @"Timestamp" ]];
+  NSArray *events = [NSMutableArray arrayWithObject:eventsDict];
+  NSDictionary *payloadDict = [[NSDictionary alloc]
+      initWithObjects:@[ self.uuid, self.appVersion, events ]
+              forKeys:@[ @"EasyAuthSessionId", @"LibVersion", @"events" ]];
+  return [NSJSONSerialization dataWithJSONObject:payloadDict
+                                         options:NSJSONWritingPrettyPrinted
+                                           error:nil];
+}
+
+@end
